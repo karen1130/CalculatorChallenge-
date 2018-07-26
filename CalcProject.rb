40a0c1c54aab364090c5b833e0f2ea7dc6af4fc3
karen1130 <41582309+karen1130@users.noreply.github.com>
2018-07-26 15:13:05 -0400

    Create CalcProject.rb

---
+++ CalcProject.rb
@@ -0,0 +1,21 @@
+
+puts "What does x equal?"
+  x = gets.strip.to_f
+puts "What does y equal?"
+  y = gets.strip.to_f
+puts "What is your equation? Choose multiply, add, subtract, divide, exponent."
+  equation = gets.strip
+  
+if equation == "multiply"  
+    puts "Your answer is #{x * y}"
+  elsif  equation == "add"
+  puts "Your answer is #{x + y}"
+  elsif  equation == "subtract"
+  puts "Your answer is #{x - y}"
+  elsif  equation == "divide"
+    puts "Your answer is #{x / y}"
+  elsif equation == "exponent"
+  puts "Your answer is #{x ** y}"
+end
+
+ 
